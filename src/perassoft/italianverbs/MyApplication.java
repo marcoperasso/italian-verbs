ff82e310d5f8a8eefe318e96e2c9b2e31806f72c
Marco <marco.perasso@gmail.com>
2013-10-22 18:00:18 +0200

    lettura verbi da .bin

--- src/perassoft/italianverbs/MyApplication.java
+++ src/perassoft/italianverbs/MyApplication.java
@@ -1,11 +1,22 @@
 package perassoft.italianverbs;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+
 import android.app.Application;
+import android.content.Context;
 
 public class MyApplication extends Application {
 	
 	private static MyApplication application;
 	private Verbs verbs;
+	private boolean verbsLoaded;
 	@Override
 	public void onCreate() {
 		MyApplication.application = this;
@@ -23,16 +34,54 @@ public class MyApplication extends Application {
 	}
 
 	public Verbs getVerbs() {
-		if (verbs == null)
-		{
+		if (verbsLoaded)
+			return verbs;
 		synchronized (this) {
-			if (verbs == null)
-			{
-				verbs = new Verbs();
-			}
-		}
+			if (verbsLoaded)
+				return verbs;
+			verbs = Verbs.loadVerbs();
+			verbsLoaded = true;
 		}
 		return verbs;
 	}
 	
+	public void saveObject(File file, Object obj) throws IOException {
+		FileOutputStream fos = new FileOutputStream(file);
+		ObjectOutput out = null;
+		try {
+			out = new ObjectOutputStream(fos);
+			out.writeObject(obj);
+			out.flush();
+		} finally {
+			out.close();
+			fos.close();
+		}
+		
+	}
+
+	public Object readObject(File file) {
+		if (file.exists()) {
+			try {
+				FileInputStream fis = new FileInputStream(file);
+				ObjectInput in = null;
+				try {
+					in = new ObjectInputStream(fis);
+					try {
+						return in.readObject();
+					} catch (Exception ex) {
+						
+					}
+				} catch (Exception e) {
+					
+				} finally {
+					in.close();
+					fis.close();
+				}
+			} catch (Exception e) {
+				
+			}
+
+		}
+		return null;
+	}
 }
--- src/perassoft/italianverbs/Verbs.java
+++ src/perassoft/italianverbs/Verbs.java
@@ -1,26 +1,24 @@
 package perassoft.italianverbs;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 
 public class Verbs extends ArrayList<Verb> {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 3464198266953132774L;
-	public Verbs() {
-		parseVerbs();
-		// loadVerbs();
+
+	private Verbs() {
 	}
 
-	private void parseVerbs() {
+	private static Verbs loadFromResource() {
 		try {
+			Verbs verbs = new Verbs();
 			InputStream is = MyApplication.getInstance().getResources()
 					.openRawResource(R.raw.verbi);
 
@@ -34,132 +32,31 @@ public class Verbs extends ArrayList<Verb> {
 					line = input.readLine();
 					verb.add(line);
 				}
-				add(verb);
+				verbs.add(verb);
 			}
 			input.close();
+			return verbs;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		return null;
 	}
 
-	private void loadVerbs() {
-		Verb verb = new Verb("essere");
-		verb.add("io sono");
-		verb.add("tu sei");
-		verb.add("egli è");
-		verb.add("noi siamo");
-		verb.add("voi siete");
-		verb.add("essi sono");
-
-		verb.add("io sono stato");
-		verb.add("tu sei stato");
-		verb.add("egli è stato");
-		verb.add("noi siamo stati");
-		verb.add("voi siete stati");
-		verb.add("essi sono stati");
-
-		verb.add("io ero");
-		verb.add("tu eri");
-		verb.add("egli era");
-		verb.add("noi eravamo");
-		verb.add("voi eravate");
-		verb.add("essi erano");
-
-		verb.add("io ero stato");
-		verb.add("tu eri stato");
-		verb.add("egli era stato");
-		verb.add("noi eravamo stati");
-		verb.add("voi eravate stati");
-		verb.add("essi erano stati");
-
-		verb.add("io fui");
-		verb.add("tu fosti");
-		verb.add("egli fu");
-		verb.add("noi fummo");
-		verb.add("voi foste");
-		verb.add("essi furono");
-
-		verb.add("io fui stato");
-		verb.add("tu fosti stato");
-		verb.add("egli fu stato");
-		verb.add("noi fummo stati");
-		verb.add("voi foste stati");
-		verb.add("essi furono stati");
-
-		verb.add("io sarò");
-		verb.add("tu sarai");
-		verb.add("egli sarà");
-		verb.add("noi saremo");
-		verb.add("voi sarete");
-		verb.add("essi saranno");
-
-		verb.add("io sarò stato");
-		verb.add("tu sarai stato");
-		verb.add("egli sarà stato");
-		verb.add("noi saremo stati");
-		verb.add("voi sarete stati");
-		verb.add("essi saranno stati");
-
-		verb.add("io sarei");
-		verb.add("tu saresti");
-		verb.add("egli sarebbe");
-		verb.add("noi saremmo");
-		verb.add("voi sareste");
-		verb.add("essi sarebbero");
-
-		verb.add("io sarei stato");
-		verb.add("tu saresti stato");
-		verb.add("egli sarebbe stato");
-		verb.add("noi saremmo stati");
-		verb.add("voi sareste stati");
-		verb.add("essi sarebbero stati");
-
-		verb.add("che io sia");
-		verb.add("che tu sia");
-		verb.add("che egli sia");
-		verb.add("che noi siamo");
-		verb.add("che voi siate");
-		verb.add("che essi siano");
-
-		verb.add("che io sia stato");
-		verb.add("che tu sia stato");
-		verb.add("che egli sia stato");
-		verb.add("che noi siamo stati");
-		verb.add("che voi siate stati");
-		verb.add("che essi siano stati");
-
-		verb.add("che io fossi");
-		verb.add("che tu fossi");
-		verb.add("che egli fosse");
-		verb.add("che noi fossimo");
-		verb.add("che voi foste");
-		verb.add("che essi fossero");
-
-		verb.add("che io fossi stato");
-		verb.add("che tu fossi stato");
-		verb.add("che egli fosse stato");
-		verb.add("che noi fossimo stati");
-		verb.add("che voi foste stati");
-		verb.add("che essi fossero stati");
-
-		verb.add("sii");
-		verb.add("sia");
-		verb.add("siamo");
-		verb.add("siate");
-		verb.add("siano");
-
-		verb.add("essere");
-
-		verb.add("essere stato");
-		verb.add("ente");
-		verb.add("stato");
-		verb.add("essendo");
-		verb.add("essendo stato");
-
-		add(verb);
-
+	public static Verbs loadVerbs() {
+		File file = MyApplication.getInstance().getFileStreamPath("Verbi.bin");
+		if (!file.exists()) {
+			Verbs verbs = loadFromResource();
+			try {
+				MyApplication.getInstance().saveObject(file, verbs);
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return verbs;
+		} else {
+			return (Verbs) MyApplication.getInstance().readObject(file);
+		}
 	}
 
 }
