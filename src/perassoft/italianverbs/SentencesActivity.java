0de43af3fff2be749ee6180c1b6e9e4e4516aa4b
marco <marco.perasso@gmail.com>
2013-11-24 21:14:13 +0100

    pubblicazione

--- src/perassoft/italianverbs/SentencesActivity.java
+++ src/perassoft/italianverbs/SentencesActivity.java
@@ -2,23 +2,21 @@ package perassoft.italianverbs;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Locale;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ContextMenu.ContextMenuInfo;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
@@ -92,6 +90,7 @@ public class SentencesActivity extends CommonActivity implements
 			InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 			imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
 
+			v.setText("");
 			return true;
 		}
 		return false;
